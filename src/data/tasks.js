f21dbc62dcd22a359701d90f5f72eb2bb4ab5e92
mateorregog <mateorrego@gmail.com>
2022-10-05 08:25:39 -0500

    First commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "mi primer tarea",
+    description: "my first task",
+  },
+  {
+    id: 1,
+    title: "mi segunda tarea",
+    description: "my 2 task",
+  },
+  {
+    id: 2,
+    title: "mi 3 tarea",
+    description: "my 3 task",
+  },
+];
